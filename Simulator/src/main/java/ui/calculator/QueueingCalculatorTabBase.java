70d8df9152b44185041860e3d5b5a0d57e44a8c6
Alexander Herzog <alexander.herzog@gmx.de>
2021-05-22 00:35:38 +0200

    Dark mode fix for queueing calculator dialog

--- Simulator/src/main/java/ui/calculator/QueueingCalculatorTabBase.java
+++ Simulator/src/main/java/ui/calculator/QueueingCalculatorTabBase.java
@@ -41,6 +41,7 @@ import javax.swing.JTextField;
 
 import language.Language;
 import systemtools.MsgBox;
+import ui.FlatLaFHelper;
 import ui.images.Images;
 
 /**
@@ -179,8 +180,15 @@ public abstract class QueueingCalculatorTabBase extends JPanel {
 	private void addTopInfo(final JPanel parent, final String text) {
 		final JPanel line;
 		parent.add(line=new JPanel(new FlowLayout(FlowLayout.LEFT)));
-		line.setBackground(Color.LIGHT_GRAY);
-		line.add(new JLabel("<html><body><b>"+text+"</b></body></html>"));
+		final String color;
+		if (FlatLaFHelper.isDark()) {
+			line.setBackground(Color.GRAY);
+			color="white";
+		} else {
+			line.setBackground(Color.LIGHT_GRAY);
+			color="black";
+		}
+		line.add(new JLabel("<html><body><span color=\""+color+"\"><b>"+text+"</b></span></body></html>"));
 		parent.add(Box.createVerticalStrut(10));
 	}
 
